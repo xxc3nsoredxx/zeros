6ce866bc9d45ce39391acc6fbc8f0e25b3543cd6
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2020-03-18 23:44:28 -0500

    Move multiboot spec stuff into header

---
+++ include/multiboot.hs
@@ -0,0 +1,46 @@
+%ifndef MULTIBOOT_HS_20200318_233525
+%define MULTIBOOT_HS_20200318_233525
+
+; helper symbols defined by the linker
+extern _mb_load_addr    ; Multiboot code + data load start address
+extern _mb_load_end_addr    ; Multiboot code + data load ends address
+extern _mb_bss_end_addr ; Multiboot bss (+stack) end address
+
+; Multiboot header structure, 4 byte aligned
+struc   mb_header_t
+    .magic: resd 1      ; Multiboot identifier
+        alignb  4
+    .flags: resd 1      ; Request features
+        alignb  4
+    .check: resd 1      ; Checksum, (magic + flags) + checksum = 0
+        alignb  4
+    .header_addr:   resd 1  ; Address of header start
+        alignb  4
+    .load_addr: resd 1  ; Address of code + data start
+        alignb  4
+    .load_end_addr: resd 1  ; Address of code + dara end
+        alignb  4
+    .bss_end_addr:  resd 1  ; Address of end of bss (and stack for us)
+        alignb  4
+    .entry_addr:    resd 1  ; Address of the entry point
+        alignb  4
+    .mode_type: resd 1  ; Graphics mode type
+        alignb  4
+    .width: resd 1      ; Screen width
+        alignb  4
+    .height:    resd 1  ; Screen height
+        alignb  4
+    .depth: resd 1      ; Pixel depth
+        alignb  4
+endstruc
+
+%assign MB_MAGIC 0x1BADB002
+%assign MB_FLAGS 0x00010003 ; Bit 2: video mode
+                            ; Bit 16: use addresses in header
+%assign MB_LOAD_END_ADDR 0  ; Loads the rest of the file
+%assign MB_GRAPHICS_MODE 1  ; Text mode
+%assign MB_SCR_WIDTH    80  ; Screen width in chars
+%assign MB_SCR_HEIGHT   25  ; Screen height in chars
+%assign MB_PIXEL_DEPTH  0   ; Pixel depth
+
+%endif
--- src/kernel0.asm
+++ src/kernel0.asm
@@ -3,6 +3,7 @@
     bits    32
 
 %include    "gdt.hs"
+%include    "multiboot.hs"
 
 global  kstart          ; Make kstart visible
 extern  kmain           ; kmain defined in kernel1.asm
@@ -24,6 +25,11 @@ kstart:
     mov esp, stack      ; Set up the stack pointer
     call    kmain       ; Kernel main function
     hlt                 ; Halt the CPU after leaving kernel
+
+section .stack nobits alloc noexec write align=16
+stack_bot:
+    resb    8 * 1024 * 1024 ; 8MiB stack
+stack:                  ; Stack pointer at highest address
 
 section .gdt progbits alloc noexec nowrite align=4
 gdt:                    ; The start of the GDT
@@ -54,54 +60,7 @@ gdt_desc:               ; GDT descriptor
         at gdt_desc_t.gdt, dd gdt
     iend
 
-section .stack nobits alloc noexec write align=16
-stack_bot:
-    resb    8 * 1024 * 1024 ; 8MiB stack
-stack:                  ; Stack pointer at highest address
-
 section .multiboot progbits align=4
-    ; helper symbols defined by the linker
-    extern _mb_load_addr        ; Multiboot code + data load start address
-    extern _mb_load_end_addr    ; Multiboot code + data load ends address
-    extern _mb_bss_end_addr     ; Multiboot bss (+stack) end address
-
-    ; Multiboot header structure, 4 byte aligned
-    struc   mb_header_t
-        .magic: resd 1  ; Multiboot identifier
-            alignb  4
-        .flags: resd 1  ; Request features
-            alignb  4
-        .check: resd 1  ; Checksum, (magic + flags) + checksum = 0
-            alignb  4
-        .header_addr:   resd 1  ; Address of header start
-            alignb  4
-        .load_addr: resd 1  ; Address of code + data start
-            alignb  4
-        .load_end_addr: resd 1  ; Address of code + dara end
-            alignb  4
-        .bss_end_addr:  resd 1  ; Address of end of bss (and stack for us)
-            alignb  4
-        .entry_addr:    resd 1  ; Address of the entry point
-            alignb  4
-        .mode_type: resd 1  ; Graphics mode type
-            alignb  4
-        .width: resd 1  ; Screen width
-            alignb  4
-        .height:    resd 1  ; Screen height
-            alignb  4
-        .depth: resd 1  ; Pixel depth
-            alignb  4
-    endstruc
-
-%assign MB_MAGIC 0x1BADB002
-%assign MB_FLAGS 0x00010003 ; Bit 2: video mode
-                            ; Bit 16: use addresses in header
-%assign MB_LOAD_END_ADDR 0  ; Loads the rest of the file
-%assign MB_GRAPHICS_MODE 1  ; Text mode
-%assign MB_SCR_WIDTH    80  ; Screen width in chars
-%assign MB_SCR_HEIGHT   25  ; Screen height in chars
-%assign MB_PIXEL_DEPTH  0   ; Pixel depth
-
 mb_header:
     istruc  mb_header_t
         at mb_header_t.magic, dd MB_MAGIC
