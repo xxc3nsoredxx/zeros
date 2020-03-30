5400071f0a4a46b2a6e0ab3978639be47cb3c8a7
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2020-03-30 02:35:15 -0500

    Basic E0 state handler

--- src/interrupts.asm
+++ src/interrupts.asm
@@ -24,6 +24,11 @@ slave_null:
     iret
 
 ; Keyboard interrupt handler
+; TODO: Finish handling all the keys
+;   Basic keys
+;   E0 keys
+;   Print screen
+;   Pause
 kb_int:
     pusha
 
@@ -31,7 +36,7 @@ kb_int:
     mov cl, [keycode.state] ; Get the current state
     cmp cl, KC_STATE_WAIT   ; New key
     jz  .new
-    cmp cl, KC_STATE_E0 ; 0xE0 prefixed key
+    cmp cl, KC_STATE_E0 ; E0 key
     jz  .e0
     cmp cl, KC_STATE_PS + 2 ; Print screen, 2 bytes left
     jz  .ps2
@@ -39,7 +44,7 @@ kb_int:
     jz  .ps1
     cmp cl, KC_STATE_REL    ; Key released
     jz  .rel
-    cmp cl, KC_STATE_E0_REL ; 0xE0 prefixed key released
+    cmp cl, KC_STATE_E0_REL ; E0 key released
     jz  .e0_rel
     cmp cl, KC_STATE_PS_REL + 3 ; Print screen released, 3 bytes left
     jz  .ps_rel3
@@ -62,8 +67,8 @@ kb_int:
     cmp cl, KC_STATE_PAUSE + 1  ; Pause, 1 bytes left
     jz  .pause1
 
-.new:
-    cmp al, 0xE0        ; New 0xE0 prefixed key
+.new:                   ; Handle new key
+    cmp al, 0xE0        ; New E0 key
     jz  .new_e0
     cmp al, 0xF0        ; New released key
     jz  .new_rel
@@ -82,21 +87,49 @@ kb_int:
 .new_pause:             ; Set pause state, 7 bytes left
     mov BYTE [keycode.state], KC_STATE_PAUSE + 7
     jmp .done
-.e0:
-.ps2:
-.ps1:
-.rel:
-.e0_rel:
-.ps_rel3:
-.ps_rel2:
-.ps_rel1:
-.pause7:
-.pause6:
-.pause5:
-.pause4:
-.pause3:
-.pause2:
-.pause1:
+
+.e0:                    ; Handle new E0 keys
+    cmp al, 0x12        ; New print screen key
+    jz  .new_ps
+    cmp al, 0xF0        ; E0 key released
+    jz  .new_e0_rel
+    mov BYTE [keycode.state], KC_STATE_WAIT ; Basic E0 key (currently ignored)
+    jmp .done
+.new_ps:                ; Set print screen 2 bytes left state
+    mov BYTE [keycode.state], KC_STATE_PS + 2
+    jmp .done
+.new_e0_rel:            ; Set E0 released state
+    mov BYTE [keycode.state], KC_STATE_E0_REL
+    jmp .done
+
+.ps2:                   ; Handle print screen, 2 bytes left
+
+.ps1:                   ; Handle print screen, 1 byte left
+
+.rel:                   ; Handle released key
+
+.e0_rel:                ; Handle relaeased E0 key
+
+.ps_rel3:               ; Handle released print screen, 3 bytes left
+
+.ps_rel2:               ; Handle released print screen, 2 bytes left
+
+.ps_rel1:               ; Handle released print screen, 1 byte left
+
+.pause7:                ; Handle pause key, 7 bytes left
+
+.pause6:                ; Handle pause key, 6 bytes left
+
+.pause5:                ; Handle pause key, 5 bytes left
+
+.pause4:                ; Handle pause key, 4 bytes left
+
+.pause3:                ; Handle pause key, 3 bytes left
+
+.pause2:                ; Handle pause key, 2 bytes left
+
+.pause1:                ; Handle pause key, 1 byte left
+
 .done:
     mov al, PIC_EOI
     out PIC_M_CMD, al
